0c26b4868abb88ad714fdb7b86fc4d9fc8f7808a
unknown <anwarg4ns@gmail.com>
2024-06-08 14:49:43 +0700

    upload pertama

---
+++ src/form.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Form(){
+    const Hours = new Date().getHours;
+    const midnight = Hours >= 24 || Hours <= 6
+    return(
+        <div>
+            
+        </div>
+    )
+}
